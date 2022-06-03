aeef6dc9561a0ea59cc8f1059f39bc73a84d4a49
DELL <DELL@DESKTOP-3NEFDR6>
2022-06-03 17:27:26 -0500

    Agregar archivos de proyecto.

---
+++ EXA/Estudiante.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EXA
+
+{
+    class Estudiante
+    {
+        string Nombre { set; get; }
+        string Apellido { set; get; }
+        string Nombre_institucion { set; get; }
+        int Cedula_identidad { set; get; }
+
+        public Estudiante (string N_ombre, string A_pellido, string I_nstitucion, int C_edula)
+        {
+            this.Nombre = N_ombre;
+            this.Apellido = A_pellido;
+            this.Nombre_institucion = I_nstitucion;
+            this.Cedula_identidad = C_edula;
+        }
+
+        public void imprimir()
+        {
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("la informacion del estudiante de primaria");
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+
+        }
+
+        
+    }
+}
---
+++ EXA/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EXA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante_Primaria = new Estudiante_Primaria();
+
+
+            
+            Estudiante__Secundaria = new Estudiante__Secundaria();
+
+
+
+            Estuadiante_Universitario = new Estuadiante_Universitario();
+
+        }
+    }
+}
